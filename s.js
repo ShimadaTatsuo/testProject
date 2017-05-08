f859500e66497255dc7ae6f958558898724f1c94
ShimadaTatsuo <tatsuo.shimada39@gmail.com>
2017-05-09 05:15:22 +0900

    Create s.js

---
+++ s.js
@@ -0,0 +1,28 @@
+var $ = require('jquery');
+
+$.fn.outputLog = function(arg) {
+    return this.each(function() {
+        (new ABC).init($(this));
+    });
+};
+
+function ABC() {
+    console.log(1);
+    this.name = 'abc';
+}
+
+ABC.prototype = $.extend({
+    init: function(elm) {
+        console.log(elm);
+        console.log(this);
+        console.log(this.name);
+    },
+    setOption: function(arg) {
+        this.option = $.extend({
+            configName: 'hoge',
+            fuga: 'fuga';
+        }, option)
+    }
+}, ABC.prototype);
+
+$('.title_h1').outputLog();
