11867648d5cb419b369ad499f8635ac47bb0829b
shimada-ta <shimada-ta@gnavi.co.jp>
2018-09-11 15:04:42 +0900

    add es6 template

---
+++ es6-template/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    'babel-polyfill',
+    path.resolve('src/js', 'index.js')
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dest/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query:{
+            presets: [
+                ['env', {'modules': false}],
+                'stage-1',
+            ],
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'css-loader',
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+  },
+  devServer: {
+    port: 3000,
+    contentBase: 'dest',
+  },
+}
